769ffd811df3d3bf86e776d27a4917250bc2df3c
João Tribuzy <joaovtribuzy@gmail.com>
2023-09-25 16:43:40 -0400

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "QuizEngine",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "QuizEngine",
+            targets: ["QuizEngine"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "QuizEngine"),
+        .testTarget(
+            name: "QuizEngineTests",
+            dependencies: ["QuizEngine"]),
+    ]
+)
